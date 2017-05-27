e2bc7c4850f6c03f0cfae2a2036a5929f844a65a
TartoGOS <draklin@live.fr>
2017-05-27 23:36:04 +0200

    Add files via upload

--- JhinTarto.lua
+++ JhinTarto.lua
@@ -1,4 +1,11 @@
-print(myHero.charName)
+if myHero.charName ~= "Jhin" then return end
+
+local menu = MenuElement({id = "JhinTarto", name = "Jhin", type = MENU})
+menu:MenuElement({id = "Combo", name = "Jhin", type = MENU})
+menu:MenuElement({id = "LaneClear", name = "LaneClear", type = MENU})
+menu:MenuElement({id = "Harass", name = "Harass", type = MENU})
+menu:MenuElement({id = "LastHit", name = "LastHit", type = MENU})
+menu:MenuElement({id = "Misc", name = "Misc", type = MENU})
 
 OnDraw(function()
 	local qRange = myHero:GetSpellData(3).range
