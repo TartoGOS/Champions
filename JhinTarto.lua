4f0c86dd1ec998c426708f6fca53bef898cbdd8f
TartoGOS <draklin@live.fr>
2017-05-27 21:43:02 +0200

    Add files via upload

--- JhinTarto.lua
+++ JhinTarto.lua
@@ -1,4 +1,7 @@
 print(myHero.charName)
+
 OnDraw(function()
-myHero:GetSpellData(3).range
-)
+	local qRange = myHero:GetSpellData(3).range
+	local qColor = Draw.Color(150,255,0,0)
+	Draw.Circle(myHero.pos, qRange, 1, qColor)
+	end)
